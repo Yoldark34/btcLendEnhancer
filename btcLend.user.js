69c802612fd588a2a1e576e6d371e7fe0809279b
Yoldark34 <yoldark@gmail.com>
2015-10-11 13:53:06 +0200

    First commit

---
+++ btcLend.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        MinerFarm
+// @namespace   MinerFarm
+// @include     http://www.minerfarm.com/*
+// @version     1.0.1
+// @grant       none
+// @require     https://cdn.rawgit.com/Yoldark34/minerFarmEnhancer/v1.0.1/libraries/jquery-1.11.1.min.js
+// ==/UserScript==
+$(document).ready(function() {
+
+});
